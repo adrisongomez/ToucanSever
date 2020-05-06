2de009d882b2a265d217d7bb76deaedff009ba98
Adrison Gomez <adrison.gomez@hotmail.com>
2020-05-06 18:37:51 -0400

    [FIX] Waring useCreate

--- src/models/db.connection.js
+++ src/models/db.connection.js
@@ -3,6 +3,7 @@ mongoose
   .connect("mongodb://localhost:27017/toucandb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("MongoDB connection was succesfully");
