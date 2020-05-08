7fb5280d3e4e08879fb3a49bd0208e5a6f6075bc
Adrison Gomez <adrison.gomez@hotmail.com>
2020-05-07 22:56:01 -0400

    [NEW] User Model Unit Testing

---
+++ src/models/user/userModel.test.js
@@ -0,0 +1,76 @@
+const faker = require("faker");
+
+const {
+  connect,
+  closeDatabase,
+  dropDatabase,
+} = require("../db.handlerForTest");
+const User = require("./user.model");
+
+const mockUserData = () => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.exampleEmail(),
+  country: faker.address.country(),
+  city: faker.address.city(),
+  state: faker.address.state(),
+  zipCode: faker.address.zipCode(),
+  address: faker.address.streetAddress(),
+});
+
+describe("User model test", () => {
+  beforeAll(() => connect());
+
+  afterAll(() => closeDatabase());
+
+  afterEach(() => dropDatabase());
+
+  it("Should create a User", () => {
+    const newUser = new User(mockUserData());
+    return User.create(newUser).then((resp) =>
+      expect(resp.firstName).toEqual(newUser.firstName)
+    );
+  });
+
+  it("Should delete a User", () => {
+    const newUser = new User(mockUserData());
+    return User.create(newUser)
+      .then((resp) => User.findByIdAndDelete(resp._id))
+      .then((resp) => User.find({}))
+      .then((resp) => expect(resp).toEqual([]));
+  });
+
+  it("Should find a User", async () => {
+    const newUser = new User(mockUserData());
+    await User.deleteMany();
+    let saveUser = await User.create(newUser);
+    // console.log(saveUser.id);
+    let findedUser = await User.findById(saveUser._id);
+    expect(findedUser.id).toBe(saveUser.id);
+  });
+
+  it("Should find many User", () => {
+    const user1 = new User(mockUserData());
+    const user2 = new User(mockUserData());
+    return User.create(user1)
+      .then(() => User.create(user2))
+      .then(() => User.find())
+      .then((resp) => expect(resp.length).toBe(2));
+  });
+
+  it("Should find update a User", () => {
+    const newUser = new User(mockUserData());
+    let idUser;
+    return newUser
+      .save()
+      .then((createUser) => {
+        createUser.firstName = "Tato";
+        createUser.lastName = "Joshua";
+        return createUser.save();
+      })
+      .then((updateUser) => {
+        expect(updateUser.firstName).toBe("Tato");
+        expect(updateUser.lastName).toBe("Joshua");
+      });
+  });
+});
