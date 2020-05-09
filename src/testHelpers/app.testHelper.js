468d029edf74e48dc313de508e6d32e538829620
Adrison Gomez <adrison.gomez@hotmail.com>
2020-05-08 23:02:32 -0400

    [NEW] Add some helpers function to Mock Database and Server only for test porpuse

---
+++ src/testHelpers/app.testHelper.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 8000;
+let server;
+
+exports.createTestApp = () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+  return app;
+};
+
+exports.listenApp = (app) => {
+  if (server !== undefined) {
+    const server = app.listen(port, () => {
+      console.log("Test Express App Running");
+    });
+  }
+};
+
+exports.getRouter = () => express.Router();
+
+exports.closeServer = () => express.closeServer;
---
+++ src/testHelpers/db.testHelper.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+const mongod = new MongoMemoryServer();
+
+exports.connect = async () => {
+  const uri = await mongod.getConnectionString();
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  };
+  await mongoose.connect(uri, mongooseOpts);
+};
+
+exports.closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  mongod.stopr();
+};
+
+exports.dropDatabase = async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+};
