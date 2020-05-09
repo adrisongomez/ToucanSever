88b5ac384347a8fdd7117e02a4fd0b7a935116d0
Adrison Gomez <adrison.gomez@hotmail.com>
2020-05-08 23:05:33 -0400

    [FIX] Refactoring Code

--- src/routes/location/location.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const LocationRoute = express.Router();
-const Location = require("../models/location/location.model");
-
-LocationRoute.get("/fake", (req, res) => {
-  const allLocation = Array.from(Array(10), () => {});
-});
