a400b06b316fdcf1a693abc847f318c45631beae
Adrison Gomez <adrison.gomez@hotmail.com>
2020-06-09 11:17:21 -0400

    [NEW] Add some new routes to Publication

--- src/services/publication/publication-service.test.js
+++ src/services/publication/publication-service.test.js
@@ -1,14 +1,22 @@
-const { createPublicationDoc } = require("./publication.service");
+const {
+  createPublicationDoc,
+  updatePublicationDoc,
+  deletePublicationDoc,
+} = require("./publication.service");
 const {
   mockUserData,
   mockPublication,
 } = require("../../__mocks__/utils.testHelper");
 
+const idAuthorValid = "123456789";
+const idPublication = "1112333456";
+const publication = mockPublication(idAuthorValid);
+const idAuthorNotValid = "1234657897897899879";
+const publicationInvalid = mockPublication(idAuthorNotValid);
+const newData = mockPublication(idAuthorValid);
+
 describe("User services create", () => {
-  const idAuthor = "123456789";
-  const idPublication = "1112333456";
-  const publication = mockPublication(idAuthor);
-  test.skip("work correctly", (done) => {
+  test("it work correctly", (done) => {
     const mockModel = {
       create: jest.fn((obj) =>
         Promise.resolve({
@@ -19,23 +27,71 @@ describe("User services create", () => {
       ),
     };
     createPublicationDoc(publication, mockModel).then((resp) => {
-      expect(resp.author).toBe(idAuthor);
+      expect(resp.author).toBe(idAuthorValid);
       expect(resp.description).toBe(publication.description);
-      expect(resp.author).toBe(idAuthor);
+      expect(resp.author).toBe(idAuthorValid);
       expect(resp._id).toBe(idPublication);
       done();
     });
   });
-  test.skip("is not working", (done) => {
-    const idAuthorNotValid = "1234657897897899879";
-    const publication = mockPublication(idAuthorNotValid);
+  test("it's not working", async () => {
     const mockModel = {
       create: (obj) => {
-        console.log("from Mock")
         return Promise.reject({ author: { message: "Author is not valid" } });
       },
     };
-    expect(createPublicationDoc(publication, mockModel)).rejects.toThrow();
-    done();
+    try {
+      await createPublicationDoc(publicationInvalid, mockModel);
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
+});
+
+describe("Publication update", () => {
+  test("it Work correctly", async () => {
+    const mockModel = {
+      updateOne: ({ _id }) =>
+        Promise.resolve({
+          populate: ([args]) => ({
+            execPopulate: () => ({ _id, ...newData }),
+          }),
+        }),
+    };
+    const data = await updatePublicationDoc(idAuthorValid, newData, mockModel);
+    expect(data.author).toBe(newData.author);
+    expect(data._id).toBe(idAuthorValid);
+  });
+
+  test("it's not working", async () => {
+    const mockModel = {
+      updateOne: ({ _id }) => Promise.reject({ error: "Reject" }),
+    };
+    try {
+      await updatePublicationDoc(idAuthorNotValid, newData, mockModel);
+    } catch (err) {
+      expect(err.error).toBe("Reject");
+    }
+  });
+});
+
+describe("Publication delete", () => {
+  test("it work correctly", async () => {
+    const mockModel = {
+      deleteOne: (args) => Promise.resolve(),
+    };
+    const { message } = await deletePublicationDoc(idAuthorValid, mockModel);
+    expect(message).toBe("Publication deleted successfully");
+  });
+  test("it doesn't work correctly", async () => {
+    const mockModel = {
+      deleteOne: (args) => Promise.reject(),
+    }
+    try{
+      await deletePublicationDoc(idAuthorNotValid, mockModel);
+    }catch(err){
+      expect(err.status).toBe(404)
+      expect(err.id).toBe(1)
+    }
   });
 });
--- src/services/publication/publication.service.js
+++ src/services/publication/publication.service.js
@@ -12,3 +12,35 @@ exports.createPublicationDoc = (publicationData, Publication) => {
       throw errors;
     });
 };
+
+exports.updatePublicationDoc = (
+  idPublication,
+  publicationNewData,
+  Publication
+) => {
+  return Publication.updateOne({ _id: idPublication }, publicationNewData).then(
+    (publicationDoc) => {
+      return publicationDoc
+        .populate("author", "firstName lastName")
+        .execPopulate();
+    }
+  );
+};
+
+exports.deletePublicationDoc = (idPublication, Publication) => {
+  return Publication.deleteOne({ _id: idPublication })
+    .then((resp) => ({
+      id: 0,
+      message: "Publication deleted successfully",
+    }))
+    .catch((err) => ({
+      status: 404,
+      message: "Publication doesn't exict",
+      id: 1,
+    }));
+};
+
+exports.findAllDoc = (Publication) => {
+  //*TODO: Make a Query for Friends*/
+  return Publication.find({}).then()
+};
