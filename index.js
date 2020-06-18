392b6aa898381552c13cfe9fb033b307ef02a5d4
Adrison Gomez <adrison.gomez@hotmail.com>
2020-06-17 21:20:10 -0400

    [NEW] setting mongoDB to deploy enviroment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+const morgan = require('morgan');
 
 //Route Declaration
 const RootRoute = require("./src/routes/root.route");
@@ -10,10 +11,11 @@ const RootRoute = require("./src/routes/root.route");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
+app.use(morgan("dev"));
 
 app.listen(port, (error) => {
   if (error) throw error;
@@ -26,4 +28,3 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/", RootRoute);
-;
