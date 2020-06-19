b5eb38b25e584fe662b3f6f94df8bf56df48d527
Adrison Gomez <adrison.gomez@hotmail.com>
2020-06-18 23:24:25 -0400

    Comment Services tested (happy and bad path)

---
+++ src/services/comment/comment-service.test.js
@@ -0,0 +1,150 @@
+const {
+  addComment,
+  deleteComment,
+  updateComment,
+} = require("./comment.service");
+const {
+  mockPublication,
+  mockCommentsData,
+} = require("../../__mocks__/utils.testHelper");
+
+describe("addComment to a publication", () => {
+  const publication = mockPublication(1);
+  const comment = mockCommentsData(1);
+
+  it("should work correctly", async () => {
+    const mockModel = {
+      findById: (_id) =>
+        Promise.resolve({
+          _id,
+          ...publication,
+          comments: {
+            create: (obj) => Promise.resolve(publication),
+          },
+        }),
+    };
+    const result = await addComment(1, comment, mockModel);
+    expect(publication).toBe(result);
+  });
+
+  it("should not work correctly, not valid author", async () => {
+    const mockModel = {
+      findById: (_id) => Promise.reject({ path: "author" }),
+    };
+    try {
+      await addComment(1, comment, mockModel);
+    } catch (error) {
+      expect(error.id).toBe(1);
+      expect(error.message).toBe("Author not valid");
+    }
+  });
+});
+
+describe("deleteComment from a publication", () => {
+  const publication = mockPublication(1);
+  const comment = mockCommentsData(1);
+
+  it("should work correctly", async () => {
+    const mockModel = {
+      findById: (_id) =>
+        Promise.resolve({
+          comments: {
+            id: (args) => ({
+              remove: () => comment,
+            }),
+          },
+          save: () => Promise.resolve(publication),
+        }),
+    };
+    const result = await deleteComment(1, 1, mockModel);
+    expect(result).toBe(publication);
+  });
+
+  it("should not work correctly, not valid publication valid", async () => {
+    const mockModel = {
+      findById: () => Promise.reject({ path: "_id" }),
+    };
+    try {
+      await deleteComment(1, 1, mockModel);
+    } catch (error) {
+      expect(error.id).toBe(1);
+      expect(error.message).toBe("Publication not exists");
+    }
+  });
+
+  it("should not work correctly, not valid comment id", async () => {
+    const mockModel = {
+      findById: () =>
+        Promise.resolve({
+          comments: {
+            id: (obj) => ({
+              remove: () => null,
+            }),
+          },
+        }),
+    };
+    try {
+      await deleteComment(1, 1, mockModel);
+    } catch (error) {
+      expect(error.id).toBe(1);
+      expect(error.message).toBe("Comment not exists");
+    }
+  });
+});
+
+describe("updateComment from a publication", () => {
+  const publication = mockPublication(1);
+  const comment = mockCommentsData(1);
+  it("should work correctly", async () => {
+    const mockModel = {
+      findById: () =>
+        Promise.resolve({
+          comments: {
+            id: (obj) => ({
+              _id: 123,
+              ...comment,
+            }),
+          },
+          save: () => {
+            publication.comments.push(comment);
+            return Promise.resolve(publication);
+          },
+        }),
+    };
+
+    const result = await updateComment(1, 1, comment, mockModel);
+    expect(result).toBe(publication);
+    expect(result.comments).toContainEqual(comment);
+  });
+
+  it("should not work correctly, PubId not valid", async () => {
+    const mockModel = {
+      findById: () => Promise.reject({ path: "_id" }),
+    };
+    try {
+      await updateComment(1, 1, comment, mockModel);
+    } catch (error) {
+      expect(error.id).toBe(1);
+      expect(error.message).toBe("Publication not exists");
+    }
+  });
+
+  it("should not work correctly, differents author", async () => {
+    const comment2 = mockCommentsData(2);
+    comment2.author = 2;
+    const mockModel = {
+      findById: () =>
+        Promise.resolve({
+          comments: {
+            id: (obj) => comment2,
+          },
+        }),
+    };
+    try {
+      await updateComment(1, 1, comment, mockModel);
+    } catch (error) {
+      expect(error.id).toBe(1);
+      expect(error.message).toBe("Not allowed, Author differents");
+    }
+  });
+});
---
+++ src/services/comment/comment.service.js
@@ -0,0 +1,51 @@
+exports.addComment = async (publicationId, comment, Publication) => {
+  try {
+    const publication = await Publication.findById(publicationId);
+    return await publication.comments.create(comment);
+  } catch (error) {
+    if (error.path === "_id") {
+      throw { id: 1, message: "Publication not exists" };
+    }
+    if (error.path === "author") {
+      throw { id: 1, message: "Author not valid" };
+    }
+    throw error;
+  }
+};
+
+exports.deleteComment = async (commentId, publicationId, Publication) => {
+  try {
+    const publication = await Publication.findById(publicationId);
+    const comment = publication.comments.id(commentId).remove();
+    if (!comment) throw { id: 1, message: "Comment not exists" };
+    return await publication.save();
+  } catch (error) {
+    if (error.path === "_id") {
+      throw { id: 1, message: "Publication not exists" };
+    }
+    throw error;
+  }
+};
+
+exports.updateComment = async (
+  commentId,
+  publicationId,
+  commentNew,
+  Publication
+) => {
+  try {
+    const publication = await Publication.findById(publicationId);
+    const comment = publication.comments.id(commentId);
+    if (!comment) throw { id: 1, message: "Comment not exists" };
+    if (comment.author !== commentNew.author) {
+      throw { id: 1, message: "Not allowed, Author differents" };
+    }
+    comment.comment = commentNew.comment;
+    return await publication.save();
+  } catch (error) {
+    if (error.path === "_id") {
+      throw { id: 1, message: "Publication not exists" };
+    }
+    throw error;
+  }
+};
